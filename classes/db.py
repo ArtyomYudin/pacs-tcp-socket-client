ede486d0111e11eb340b04723737e80d175bcdfe
Artyom Yudin <a.yudin@center-inform.ru>
2024-11-07 10:55:21 +0300

    fix db class, add pool

--- classes/db.py
+++ classes/db.py
@@ -1,4 +1,6 @@
 import asyncpg
+from celery.bin.result import result
+
 
 class DB:
     def __init__(self, user, password, host, database):
@@ -6,27 +8,25 @@ class DB:
         self.password = password
         self.host = host
         self.database = database
-        self.connection = None
-
-        #asyncio.run(self.connect())
+        self.pool = None
 
     async def connect(self):
-            self.connection = await asyncpg.connect(user=self.user, password=self.password,
+            self.pool = await asyncpg.create_pool(user=self.user, password=self.password,
                                                     database=self.database, host=self.host)
 
     async def execute(self, query, *args):
-        if self.connection is None:
-            self.connection = await asyncpg.connect(user=self.user, password=self.password,
+        if self.pool is None:
+            self.pool = await asyncpg.create_pool(user=self.user, password=self.password,
                                                     database=self.database, host=self.host)
-        result = await self.connection.execute(query, *args)
-        return result
+        async with self.pool.acquire() as conn:
+            return await conn.execute(query, *args)
 
     async def fetch_row(self, query, *args):
-        if self.connection is None:
+        if self.pool is None:
             await self.connect()
-        result = await self.connection.fetchrow(query, *args)
-        return result
+        async with self.pool.acquire() as conn:
+            return await conn.fetchrow(query, *args)
 
     async def close(self):
-        if self.connection is not None:
-            await self.connection.close()
+        if self.pool is not None:
+            await self.pool.close()
